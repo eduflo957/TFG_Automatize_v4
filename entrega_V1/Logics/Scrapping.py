83bbc0de37ff198f940bc16e7c586e2a6faef4ce
BungaEdu <eduardoflores.9595@gmail.com>
2023-05-08 22:48:12 +0200

    V1: he intentado quitar el primer pop up de las páginas, pero es muy complicado, recuerda que la versión anterior es más simple

--- entrega_V1/Logics/Scrapping.py
+++ entrega_V1/Logics/Scrapping.py
@@ -7,17 +7,25 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 
-def doScrappingWeb(columna, stringClasePom):
+def doScrappingWeb(columna, busquedaXpath, botonSiNo, busquedaXpathPopup, dominio):
+    botonSiNo = True
     driver = webdriver.Chrome()
+
+    if botonSiNo == True:
+        driver.get(dominio)
+        driver.maximize_window()
+        wait = WebDriverWait(driver, 3)
+        buttonPopUp = wait.until(EC.presence_of_element_located((By.XPATH, busquedaXpathPopup)))
+
+    time.sleep(5)
 
     for enlace in columna:
         driver.get(enlace)
         driver.maximize_window()
         wait = WebDriverWait(driver, 3)
-        button = wait.until(EC.presence_of_element_located((By.XPATH, stringClasePom)))
+        button = wait.until(EC.presence_of_element_located((By.XPATH, busquedaXpath)))
         time.sleep(2)
         button.click()
         time.sleep(2)
-
 
     driver.quit()
